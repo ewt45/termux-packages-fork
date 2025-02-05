a27230c5c064904e93421e2766df538071b49169
ewt45 <79033456+ewt45@users.noreply.github.com>
2025-02-05 21:36:03 +0800

    Update build.sh

--- x11-packages/termux-x11-nightly/build.sh
+++ x11-packages/termux-x11-nightly/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/termux-x11
 TERMUX_PKG_DESCRIPTION="Termux X11 add-on."
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Twaik Yont @twaik"
-TERMUX_PKG_VERSION=1.03.01
-TERMUX_PKG_REVISION=3
-TERMUX_PKG_SRCURL=https://github.com/termux/termux-x11/archive/efb2d97a46adf1657bacf395c38166a648260066.tar.gz
-TERMUX_PKG_SHA256=49f635b0aa731b72ad5f5fb78f770d47bc86eeab37b80a2e7a31e93e77e183b3
+TERMUX_PKG_VERSION=99.00.00
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://github.com/ewt45/termux-x11-fork/archive/5157eeda05c66c4ab00def4e31e09150ecb87006.tar.gz
+TERMUX_PKG_SHA256=50ac09363fcc74a1905a24a31588932c351a0dea83948ab3039f0128034d8506
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -20,7 +20,7 @@ termux_step_make() {
 
 termux_step_make_install() {
 	# Downloading full JDK to compile 7kb apk seems excessive, let's download a prebuilt.
-	local LOADER_URL="https://github.com/termux/termux-x11/releases/download/nightly/termux-x11-nightly-1.03.01-0-any.pkg.tar.xz"
+	local LOADER_URL="https://github.com/ewt45/termux-x11-fork/releases/download/aar-250203/termux-x11-nightly-99.00.00-any.pkg.tar.xz"
 	install -t $TERMUX_PREFIX/bin -m 755 termux-x11 termux-x11-preference
 	mkdir -p $TERMUX_PREFIX/libexec/termux-x11
 	wget -qO- $LOADER_URL | tar -OJxf - --wildcards "*loader.apk" > $TERMUX_PREFIX/libexec/termux-x11/loader.apk
